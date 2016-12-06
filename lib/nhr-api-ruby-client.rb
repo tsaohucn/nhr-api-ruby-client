2631ef317813d3b089758eb79907960b1678092b
kaogaau <kaogaau@gmail.com>
2016-12-06 11:04:15 +0800

    modify nhr token fails error message

--- lib/nhr-api-ruby-client.rb
+++ lib/nhr-api-ruby-client.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'rest-client'
+
 module Nhr
   class Client
 
@@ -20,9 +23,9 @@ module Nhr
       parameter = {token: @token, gateway_id: @gateway_id, action: @action, stime: @stime, etime: @etime, device_ieee: @device_ieee, cmdvalue: @cmdvalue}
       formdata = URI.encode_www_form(parameter)
       response = RestClient::Resource.new(@nhr_url, :ssl_version => 'TLSv1').post(formdata)  
-      raise "response nil" if response == nil
+      raise "response nil，it should must be a json string and could be gsub" unless response
       response = JSON.parse(response.split(';')[-1].gsub("\r\n\t\r\n?>\t",'')) 
-      raise "#{response},gateway_id: #{@gateway_id}" unless response['status'] == true or (@action = 'GetPIRMotionStatus' && response['response'] == 'not found data!!') 
+      raise "#{response['response']}" unless response['status'] == true or (@action = 'GetPIRMotionStatus' && response['response'] == 'not found data!!') 
       @response = response
   	end
   end
