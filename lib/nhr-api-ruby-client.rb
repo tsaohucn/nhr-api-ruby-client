2e53afd934c1e317a93ee97671bd9a7c19dac9a1
Tsao <tsaohucn@gmail.com>
2017-12-14 00:02:49 +0800

    adjustment format

--- lib/nhr-api-ruby-client.rb
+++ lib/nhr-api-ruby-client.rb
@@ -5,21 +5,21 @@ module Nhr
   class Client
 
     attr_accessor :response
-	  attr_reader :nhr_url, :token, :gateway_id, :action, :stime, :etime, :device_ieee, :cmdvalue
+    attr_reader :nhr_url, :token, :gateway_id, :action, :stime, :etime, :device_ieee, :cmdvalue
 
-  	def initialize(nhr_url: nil, token: nil, gateway_id: nil, device_ieee: nil, cmdvalue: nil, action: nil, stime: nil, etime: nil)
-  	  @nhr_url = nhr_url
-  	  @token = token
-  	  @gateway_id = gateway_id
+    def initialize(nhr_url: nil, token: nil, gateway_id: nil, device_ieee: nil, cmdvalue: nil, action: nil, stime: nil, etime: nil)
+      @nhr_url = nhr_url
+      @token = token
+      @gateway_id = gateway_id
       @device_ieee = device_ieee
-  	  @action = action
-  	  @stime = stime
-  	  @etime = etime
+      @action = action
+      @stime = stime
+      @etime = etime
       @cmdvalue = cmdvalue
-  	  client
-  	end
+      client
+    end
 
-  	def client
+    def client
       parameter = {token: @token, gateway_id: @gateway_id, action: @action, stime: @stime, etime: @etime, device_ieee: @device_ieee, cmdvalue: @cmdvalue}
       formdata = URI.encode_www_form(parameter)
       response = RestClient::Resource.new(@nhr_url).post(formdata)  
@@ -27,6 +27,6 @@ module Nhr
       response = JSON.parse(response.split(';')[-1].gsub("\r\n\t\r\n?>\t",'')) 
       raise "#{response['response']}" unless response['status'] == true or (@action = 'GetPIRMotionStatus' && response['response'] == 'not found data!!') 
       @response = response
-  	end
+    end
   end
 end
